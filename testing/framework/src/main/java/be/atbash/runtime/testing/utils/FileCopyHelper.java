1e0b23eb207e77572cab59630ad33ee044a34890
Rudy De Busscher <rdebusscher@gmail.com>
2022-01-05 19:38:59 +0100

    Fix issue where FROM in custom docker images isn't updated (Testing framework)

--- testing/framework/src/main/java/be/atbash/runtime/testing/utils/FileCopyHelper.java
+++ testing/framework/src/main/java/be/atbash/runtime/testing/utils/FileCopyHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Rudy De Busscher (https://www.atbash.be)
+ * Copyright 2021-2022 Rudy De Busscher (https://www.atbash.be)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,7 +103,7 @@ public class FileCopyHelper {
             Files.find(Paths.get(resource.toURI()),
                             Integer.MAX_VALUE,
                             (filePath, fileAttr) -> fileAttr.isRegularFile()
-                                    && !filePath.getFileName().endsWith("DockerFile"))
+                                    && !filePath.getFileName().endsWith("Dockerfile"))
                     .forEach(p -> copyToTemp(p, tempDir));
         } catch (IOException | URISyntaxException e) {
             Assertions.fail(e.getMessage());
