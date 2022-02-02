56773e6ea22446d1032f67cc98274a752cebbada
Rudy De Busscher <rdebusscher@gmail.com>
2022-02-02 20:02:34 +0100

    Support Configuration file within Docker

--- runtime-main/src/main/docker-jdk17/bin/entrypoint.sh
+++ runtime-main/src/main/docker-jdk17/bin/entrypoint.sh
@@ -5,4 +5,9 @@ if [ ${STATELESS} = 'true' ]
 then
    ATBASH_ARGS="${ATBASH_ARGS} --stateless --no-logToFile"
 fi
+if [ -f "${CONFIG_FILE_LOCATION}" ]
+then
+   ATBASH_ARGS="${ATBASH_ARGS} -c ${CONFIG_FILE_LOCATION}"
+fi
+
 exec java -XX:MaxRAMPercentage=${MEM_MAX_RAM_PERCENTAGE} -Xss${MEM_XSS} -XX:+UseContainerSupport ${JVM_ARGS} -jar atbash-runtime.jar --logToConsole --deploymentdirectory ${DEPLOYMENT_DIR} ${ATBASH_ARGS}
